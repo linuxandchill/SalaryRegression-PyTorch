08f95d0253871cec2347916f40d10eb118cd3af2
linuxandchill <rivercodi@gmail.com>
2018-07-12 12:03:11 -0700

    typos

--- simpleReg.py
+++ simpleReg.py
@@ -15,7 +15,7 @@ y_temp = dataset.iloc[:, 1:].values
 X_train = torch.FloatTensor(x_temp)
 Y_train = torch.FloatTensor(y_temp)
 
-#### MODLE ARCHITECTURE #### 
+#### MODEL ARCHITECTURE #### 
 
 class Model(torch.nn.Module):
     def __init__(self):
@@ -36,17 +36,11 @@ for epoch in range(400):
     y_pred = model(X_train)
 
     loss = loss_func(y_pred, Y_train)
-    print(epoch,loss.data.item())
-    #print(loss.data[0].item())
-    #plt.scatter(epoch, loss.data[0].item(), color='r', s=10, marker='o')
 
     optimizer.zero_grad()
     loss.backward()
     optimizer.step()
 
-#plt.xlabel("Epochs")
-##plt.ylabel("Epochs")
-#plt.show()
 
 test_exp = torch.FloatTensor([[6.0]])
 print("If u have 6 yrs exp, Salary is:", model(test_exp).data[0][0].item())
